d716f016fac708f84498f86715e001401571ceb9
= <=>
2021-01-07 11:12:40 -0100

    ADded dispose method, implemented it plus the adquire method

--- src/ObjectPool.java
+++ src/ObjectPool.java
@@ -2,11 +2,16 @@ public class ObjectPool {
 
     private static ObjectPool objectPoolInstance;
 
+    private Object[] objects;
+    private final int MAX_OBJECTS = 10;
+    private final int MIN_OBJECTS = 0;
+
     public ObjectPool() {
         if (objectPoolInstance != null) {
             return;
         }
 
+        objects = new Object[MAX_OBJECTS];
         objectPoolInstance = this;
     }
 
@@ -14,7 +19,13 @@ public class ObjectPool {
         return objectPoolInstance;
     }
 
-    public static void adquire() {
+    public static void adquire(Object obj, int index) {
+        objectPoolInstance.objects[index] = obj;
+    }
 
+    public static Object dispose(int index) {
+        Object obj = objectPoolInstance.objects[index];
+        objectPoolInstance.objects[index] = null;
+        return obj;
     }
 }
