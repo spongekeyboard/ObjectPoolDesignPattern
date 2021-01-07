5cac2151fcbabe9494825e72464430fab218ebb7
= <=>
2021-01-07 11:23:33 -0100

    Quickfix to code. Code complete.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        ObjectPool.getInstance().adquire(new String("Leandro Jesus"), 1);
+        System.out.println(ObjectPool.getInstance().dispose(1));
     }
 
 }
--- src/ObjectPool.java
+++ src/ObjectPool.java
@@ -2,30 +2,29 @@ public class ObjectPool {
 
     private static ObjectPool objectPoolInstance;
 
-    private Object[] objects;
-    private final int MAX_OBJECTS = 10;
-    private final int MIN_OBJECTS = 0;
+    private static Object[] objects;
+    private static final int MAX_OBJECTS = 10;
+    private static int MIN_OBJECTS = 0;
 
     public ObjectPool() {
-        if (objectPoolInstance != null) {
-            return;
-        }
-
         objects = new Object[MAX_OBJECTS];
-        objectPoolInstance = this;
     }
 
     public static ObjectPool getInstance() {
+        if (objectPoolInstance != null) {
+            return objectPoolInstance;
+        }
+
+        objectPoolInstance = new ObjectPool();
         return objectPoolInstance;
     }
 
-    public static void adquire(Object obj, int index) {
-        objectPoolInstance.objects[index] = obj;
+    public void adquire(Object obj, int index) {
+        objects[index] = obj;
     }
 
-    public static Object dispose(int index) {
-        Object obj = objectPoolInstance.objects[index];
-        objectPoolInstance.objects[index] = null;
+    public Object dispose(int index) {
+        Object obj = objects[index];
         return obj;
     }
 }
